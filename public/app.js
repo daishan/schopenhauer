c5b3f345bb0d4f6e799e0802ff12d35f24b6c2f9
Andreas Hemel <dai.shan@gmx.net>
2015-07-15 00:54:49 +0200

    gh-pages update

--- public/app.js
+++ public/app.js
@@ -168,13 +168,12 @@
 var audio = (function ($) {
     "use strict";
 
-    var stopCallback;
     var currentPlaylist;
     var titleOverride;
 
     var audioElement = $('audio').get(0);
 
-    function init(onStop) {
+    function init() {
         console.log('audio.init()');
         $('audio')
             .on('timeupdate', function (ev) {
@@ -183,7 +182,6 @@ var audio = (function ($) {
             })
             .on('ended', function (ev) {
                 console.log('ended', ev, this);
-                //stopCallback();
                 toggleButtonState($('#play-button'), false);
             });
 
@@ -197,12 +195,10 @@ var audio = (function ($) {
             console.log('seek', seekFraction, seekPos);
             audioElement.currentTime = seekPos;
         });
-
-        stopCallback = onStop;
     }
 
     function play() {
-        //var audioElement = $('audio').get(0);
+        toggleButtonState($('#play-button'), true);
         if (audioElement.paused) {
             audioElement.play();
             return true;
@@ -215,6 +211,9 @@ var audio = (function ($) {
     function updateSeekBar(played) {
         var width = 200;
         var pos = played * width;
+        if (isNaN(pos)) {
+            pos = 0;
+        }
         $('.seekbar-marker')
             .attr('x1', pos)
             .attr('x2', pos);
@@ -237,6 +236,7 @@ var audio = (function ($) {
                 .find('.tracklength').text(entry.length).end()
                 .click(function () {
                     selectEntry.call(this, entry);
+                    play();
                 })
                 .appendTo($list);
         });
@@ -312,7 +312,7 @@ var schop = (function ($) {
     }
 
     function showSingleHeadline(text, index) {
-        var subtext = index ? nodetexts[index].sub : '';
+        var subtext = index === 0 || index ? nodetexts[index].sub : '';
         $('.topic-single')
             .find('h1').html(text).end()
             .find('h2').html(subtext).end()
@@ -468,6 +468,7 @@ var schop = (function ($) {
         audio.reset();
         toggleButtonState($('#text-button'), false);
         toggleButtonState($('#music-button'), false);
+        $('#info-button').css('visibility', 'hidden');
     }
 
     function toggleSingleTopic(i) {
@@ -501,6 +502,7 @@ var schop = (function ($) {
 
     function loadContentWithTitle(title, section, code) {
         showSingleHeadline(title);
+        resetAudioAndButtons();
         loadContentOnly(section, code);
     }
 
@@ -639,42 +641,42 @@ var musicPlaylists = {
 };
 
 var speakerPlaylists = {
-    '1': [{'file': 'speaker/01_SatzVomGrunde_WeltAlsVorstellung.mp3', 'length': '9:59'}],
-    '2': [{'file': 'speaker/02_SatzVomGrunde_WeltAlsWille.mp3', 'length': '9:59'}],
-    '3': [{'file': 'speaker/03_SatzVomGrunde_MetaphysikDesSchoenen.mp3', 'length': '9:59'}],
-    '4': [{'file': 'speaker/04_SatzVomGrunde_BejahungVerneinung.mp3', 'length': '9:59'}],
-    '5': [{'file': 'speaker/05_SatzVomGrunde_GuteSchlechteMusik.mp3', 'length': '9:59'}],
-    '6': [{'file': 'speaker/06_SatzVomGrunde_EntzweiungVersoehnung.mp3', 'length': '9:59'}],
-    '7': [{'file': 'speaker/07_SatzVomGrunde_KlassischePopulaereMusik.mp3', 'length': '9:59'}],
-    '8': [{'file': 'speaker/08_WeltAlsVorstellung_WeltAlsWille.mp3', 'length': '9:59'}],
-    '9': [{'file': 'speaker/09_WeltAlsVorstellung_MetaphysikDesSchoenen.mp3', 'length': '9:59'}],
-    '10': [{'file': 'speaker/10_WeltAlsVorstellung_BejahungVerneinung.mp3', 'length': '9:59'}],
-    '11': [{'file': 'speaker/11_WeltAlsVorstellung_GuteSchlechteMusik.mp3', 'length': '9:59'}],
-    '12': [{'file': 'speaker/12_WeltAlsVorstellung_EntzweiungVersoehnung.mp3', 'length': '9:59'}],
-    '13': [{'file': 'speaker/13_WeltAlsVorstellung_KlassischePopulaereMusik.mp3', 'length': '9:59'}],
-    '14': [{'file': 'speaker/14_WeltAlsWille_MetaphysikDesSchoenen.mp3', 'length': '9:59'}],
-    '15': [{'file': 'speaker/15_WeltAlsWille_BejahungVerneinung.mp3', 'length': '9:59'}],
-    '16': [{'file': 'speaker/16_WeltAlsWille_GuteSchlechteMusik.mp3', 'length': '9:59'}],
-    '17': [{'file': 'speaker/17_WeltAlsWille_EntzweiungVersoehnung.mp3', 'length': '9:59'}],
-    '18': [{'file': 'speaker/18_WeltAlsWille_KlassischePopulaereMusik.mp3', 'length': '9:59'}],
-    '19': [{'file': 'speaker/19_MetaphysikDesSchoenen_BejahungVerneinung.mp3', 'length': '9:59'}],
-    '20': [{'file': 'speaker/20_MetaphysikDesSchoenen_GuteSchlechteMusik.mp3', 'length': '9:59'}],
-    '21': [{'file': 'speaker/21_MetaphysikDesSchoenen_EntzweiungVersoehnung.mp3', 'length': '9:59'}],
-    '22': [{'file': 'speaker/22_MetaphysikDesSchoenen_KlassischePopulaereMusik.mp3', 'length': '9:59'}],
-    '23': [{'file': 'speaker/23_BejahungVerneinung_GuteSchlechteMusik.mp3', 'length': '9:59'}],
-    '24': [{'file': 'speaker/24_BejahungVerneinung_EntzweiungVersoehnung.mp3', 'length': '9:59'}],
-    '25': [{'file': 'speaker/25_BejahungVerneinung_KlassischePopulaereMusik.mp3', 'length': '9:59'}],
-    '26': [{'file': 'speaker/26_GuteSchlechteMusik_EntzweiungVersoehnung.mp3', 'length': '9:59'}],
-    '27': [{'file': 'speaker/27_GuteSchlechteMusik_KlassischePopulaereMusik.mp3', 'length': '9:59'}],
-    '28': [{'file': 'speaker/28_EntzweiungVersoehnung_KlassischePopulaereMusik.mp3', 'length': '9:59'}],
-    'A': [{'file': 'speaker/A_SatzVomGrunde.mp3', 'length': '9:59'}],
-    'B': [{'file': 'speaker/B_WeltAlsVorstellung.mp3', 'length': '9:59'}],
-    'C': [{'file': 'speaker/C_WeltAlsWille.mp3', 'length': '9:59'}],
-    'D': [{'file': 'speaker/D_MetaphysikDesSchoenen.mp3', 'length': '9:59'}],
-    'E': [{'file': 'speaker/E_BejahungVerneinung.mp3', 'length': '9:59'}],
-    'F': [{'file': 'speaker/F_GuteSchlechteMusik.mp3', 'length': '9:59'}],
-    'G': [{'file': 'speaker/G_EntzweiungVersoehnung.mp3', 'length': '9:59'}],
-    'H': [{'file': 'speaker/H_KlassischePopulaereMusik.mp3', 'length': '9:59'}]
+    '1': [{'file': 'speaker/01_SatzVomGrunde_WeltAlsVorstellung.mp3', 'length': '3:11'}],
+    '2': [{'file': 'speaker/02_SatzVomGrunde_WeltAlsWille.mp3', 'length': '2:12'}],
+    '3': [{'file': 'speaker/03_SatzVomGrunde_MetaphysikDesSchoenen.mp3', 'length': '2:45'}],
+    '4': [{'file': 'speaker/04_SatzVomGrunde_BejahungVerneinung.mp3', 'length': '1:47'}],
+    '5': [{'file': 'speaker/05_SatzVomGrunde_GuteSchlechteMusik.mp3', 'length': '2:48'}],
+    '6': [{'file': 'speaker/06_SatzVomGrunde_EntzweiungVersoehnung.mp3', 'length': '2:34'}],
+    '7': [{'file': 'speaker/07_SatzVomGrunde_KlassischePopulaereMusik.mp3', 'length': '3:17'}],
+    '8': [{'file': 'speaker/08_WeltAlsVorstellung_WeltAlsWille.mp3', 'length': '3:13'}],
+    '9': [{'file': 'speaker/09_WeltAlsVorstellung_MetaphysikDesSchoenen.mp3', 'length': '2:38'}],
+    '10': [{'file': 'speaker/10_WeltAlsVorstellung_BejahungVerneinung.mp3', 'length': '2:15'}],
+    '11': [{'file': 'speaker/11_WeltAlsVorstellung_GuteSchlechteMusik.mp3', 'length': '2:33'}],
+    '12': [{'file': 'speaker/12_WeltAlsVorstellung_EntzweiungVersoehnung.mp3', 'length': '3:06'}],
+    '13': [{'file': 'speaker/13_WeltAlsVorstellung_KlassischePopulaereMusik.mp3', 'length': '2:18'}],
+    '14': [{'file': 'speaker/14_WeltAlsWille_MetaphysikDesSchoenen.mp3', 'length': '2:18'}],
+    '15': [{'file': 'speaker/15_WeltAlsWille_BejahungVerneinung.mp3', 'length': '3:08'}],
+    '16': [{'file': 'speaker/16_WeltAlsWille_GuteSchlechteMusik.mp3', 'length': '3:02'}],
+    '17': [{'file': 'speaker/17_WeltAlsWille_EntzweiungVersoehnung.mp3', 'length': '2:28'}],
+    '18': [{'file': 'speaker/18_WeltAlsWille_KlassischePopulaereMusik.mp3', 'length': '2:56'}],
+    '19': [{'file': 'speaker/19_MetaphysikDesSchoenen_BejahungVerneinung.mp3', 'length': '3:07'}],
+    '20': [{'file': 'speaker/20_MetaphysikDesSchoenen_GuteSchlechteMusik.mp3', 'length': '2:09'}],
+    '21': [{'file': 'speaker/21_MetaphysikDesSchoenen_EntzweiungVersoehnung.mp3', 'length': '3:02'}],
+    '22': [{'file': 'speaker/22_MetaphysikDesSchoenen_KlassischePopulaereMusik.mp3', 'length': '2:21'}],
+    '23': [{'file': 'speaker/23_BejahungVerneinung_GuteSchlechteMusik.mp3', 'length': '2:17'}],
+    '24': [{'file': 'speaker/24_BejahungVerneinung_EntzweiungVersoehnung.mp3', 'length': '2:21'}],
+    '25': [{'file': 'speaker/25_BejahungVerneinung_KlassischePopulaereMusik.mp3', 'length': '3:07'}],
+    '26': [{'file': 'speaker/26_GuteSchlechteMusik_EntzweiungVersoehnung.mp3', 'length': '2:24'}],
+    '27': [{'file': 'speaker/27_GuteSchlechteMusik_KlassischePopulaereMusik.mp3', 'length': '3:11'}],
+    '28': [{'file': 'speaker/28_EntzweiungVersoehnung_KlassischePopulaereMusik.mp3', 'length': '2:12'}],
+    'A': [{'file': 'speaker/A_SatzVomGrunde.mp3', 'length': '3:49'}],
+    'B': [{'file': 'speaker/B_WeltAlsVorstellung.mp3', 'length': '3:11'}],
+    'C': [{'file': 'speaker/C_WeltAlsWille.mp3', 'length': '4:15'}],
+    'D': [{'file': 'speaker/D_MetaphysikDesSchoenen.mp3', 'length': '3:51'}],
+    'E': [{'file': 'speaker/E_BejahungVerneinung.mp3', 'length': '4:28'}],
+    'F': [{'file': 'speaker/F_GuteSchlechteMusik.mp3', 'length': '3:34'}],
+    'G': [{'file': 'speaker/G_EntzweiungVersoehnung.mp3', 'length': '3:14'}],
+    'H': [{'file': 'speaker/H_KlassischePopulaereMusik.mp3', 'length': '3:13'}]
 };
 
 var plusmenu = (function ($, schop) {
