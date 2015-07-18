484c2aca2cf4dff6cd8e35232eb15a3a312aa65a
Andreas Hemel <dai.shan@gmx.net>
2015-07-18 11:16:24 +0200

    add music

--- app/scripts/playlists.js
+++ app/scripts/playlists.js
@@ -1,7 +1,114 @@
 var musicPlaylists = {
+    '1': [{title: 'Supertramp - The Logical Song', file: 'music/01_Supertramp - The Logical Song.mp3', length: '4:05'}],
+    '2': [{title: 'Muse - Hysteria', file: 'music/02_Muse - Hysteria.mp3', length: '3:46'}],
+    '3': [{title: 'Alan Hovhaness - And God created Great Whales', file: 'music/03_Alan Hovhaness - And God created Great Whales.mp3', length: '12:16'}],
+    '4': [{title: 'Pocahontas - Was das nächste Ufer bringt', file: 'music/04_Pocahontas - Was das nächste Ufer bringt.mp3', length: '2:27'}],
+    '5': [{title: 'Sigur Ros - Untitled 4', file: 'music/05_Sigur Ros - Untitled 4.mp3', length: '7:33'}],
+    '6': [{
+        title: 'Philipp Poisel - Für keine Kohle dieser Welt (Projekt Seerosenteich)',
+        file: 'music/06_Philipp Poisel - Für keine Kohle dieser Welt (Projekt Seerosenteich).mp3',
+        length: '4:42'
+    }],
+    '7': [
+        {
+            title: 'Gioachino Rossini  - Il Barbiere di Siviglia - Largo al factotum',
+            file: 'music/07_Gioachino Rossini  - Il Barbiere di Siviglia - Largo al factotum.mp3',
+            length: '4:58'
+        },
+        {title: 'Katy Perry - Wide awake', file: 'music/07_Katy Perry - Wide awake.mp3', length: '3:40'},
+        {title: 'Mariah Carey - Heartbreaker', file: 'music/07_Mariah Carey - Heartbreaker.mp3', length: '4:48'}
+    ],
+    '8': [{title: 'System of a Down - Science', file: 'music/08_System of a Down - Science.mp3', length: '2:42'}],
+    '9': [{title: 'Sergei Rachmaninoff - Die Toteninsel', file: 'music/09_Sergei Rachmaninoff - Die Toteninsel.mp3', length: '21:39'}],
+    '10': [{title: 'Natasha Bedingfield - Unwritten', file: 'music/10_Natasha Bedingfield - Unwritten.mp3', length: '3:41'}],
+    '11': [{
+        title: 'Joseph Haydn - Die Jahreszeiten- Der Sommer - Ach das Ungewitter naht',
+        file: 'music/11_Joseph Haydn - Die Jahreszeiten- Der Sommer - Ach das Ungewitter naht.mp3',
+        length: '3:42'
+    }],
+    '12': [{title: 'Julie Andrews - Do Re Mi (Sound of Music)', file: 'music/12_Julie Andrews - Do Re Mi (Sound of Music).mp3', length: '5:35'}],
+    '13': [
+        {title: 'Carl Orff - Carmina Burana - O Fortuna', file: 'music/13_Carl Orff - Carmina Burana - O Fortuna.mp3', length: '5:21'},
+        {title: 'Nas - Hate Me Now (feat. Puff Daddy)', file: 'music/13_Nas - Hate Me Now (feat. Puff Daddy).mp3', length: '4:44'}
+    ],
+    '14': [{title: 'Katy Perry - Wide awake', file: 'music/14_Katy Perry - Wide awake.mp3', length: '3:40'}],
+    '15': [
+        {title: 'CocoRosie - Tearz for Animals', file: 'music/15_CocoRosie - Tearz for Animals.mp3', length: '4:16'},
+        {title: 'Gustav Mahler - Sinfonie 3 - Oh Mensch gib Acht', file: 'music/15_Gustav Mahler - Sinfonie 3 - Oh Mensch gib Acht.mp3', length: '9:17'}
+    ],
+    '16': [{title: 'Franz Schubert - An Die Musik', file: 'music/16_Franz Schubert - An Die Musik.mp3', length: '2:52'}],
+    '17': [{title: 'Son House - Death Letter Blues', file: 'music/17_Son House - Death Letter Blues.mp3', length: '4:21'}],
+    '18': [{title: 'Erik Satie - Je te veux', file: 'music/18_Erik Satie - Je te veux.mp3', length: '4:21'}],
+    '19': [{title: 'Miley Cyrus - Wrecking Ball', file: 'music/19_Miley Cyrus - Wrecking Ball.mp3', length: '3:41'}],
+    '20': [{title: 'Stevie Wonder - Sir Duke', file: 'music/20_ Stevie Wonder - Sir Duke.mp3', length: '3:52'}],
+    '21': [{title: 'David Bowie - Thru These Architects Eyes', file: 'music/21_David Bowie - Thru These Architects Eyes.mp3', length: '4:34'}],
+    '22': [
+        {
+            title: 'Richard Strauss - Eine Alpensinfonie - Sonnenaufgang',
+            file: 'music/22_Richard Strauss - Eine Alpensinfonie - Sonnenaufgang.mp3',
+            length: '1:30'
+        },
+        {title: 'The Beatles - Lucy in the sky with diamonds', file: 'music/22_The Beatles - Lucy in the sky with diamonds.mp3', length: '4:13'}
+    ],
+    '23': [
+        {title: 'Guiseppe Verdi - Messa da Requiem - Dies Irae', file: 'music/23_Guiseppe Verdi - Messa da Requiem - Dies Irae.mp3', length: '2:27'},
+        {title: 'James Blake - Limit To Your Love', file: 'music/23_James Blake - Limit To Your Love.mp3', length: '3:18'},
+        {
+            title: 'Wolfgang Amadeus Mozart - Requiem in d-Moll - Lacrimosa',
+            file: 'music/23_Wolfgang Amadeus Mozart - Requiem in d-Moll - Lacrimosa.mp3',
+            length: '3:22'
+        }
+    ],
+    '24': [{
+        title: 'Gioachino Rossini - Petite Messe Solennelle - Cum sancto Spiritu',
+        file: 'music/24_Gioachino Rossini - Petite Messe Solennelle - Cum sancto Spiritu.mp3',
+        length: '5:48'
+    }],
+    '25': [
+        {title: 'Marvin Gaye - Lets Get It On', file: 'music/25_Marvin Gaye - Lets Get It On.mp3', length: '3:58'},
+        {
+            title: 'Richard Wagner - Tristan und Isolde - Isoldes Liebestod',
+            file: 'music/25_Richard Wagner - Tristan und Isolde - Isoldes Liebestod.mp3',
+            length: '7:25'
+        }
+    ],
+    '26': [
+        {title: '3 Penny Choir and Orchestra - Call me maybe', file: 'music/26_3 Penny Choir and Orchestra - Call me maybe.mp3', length: '3:41'},
+        {title: 'JIMEK - Hip Hop History orchestrated', file: 'music/26_JIMEK - Hip Hop History orchestrated.mp3', length: '8:13'}
+    ],
+    '27': [
+        {title: 'Kanye West - I Am A God', file: 'music/27_Kanye West - I Am A God.mp3', length: '3:51'},
+        {title: 'Richard Wagner - Die Walküre - So nimm meinen Segen', file: 'music/27_Richard Wagner - Die Walküre - So nimm meinen Segen.mp3', length: '4:53'}
+    ],
+    '28': [
+        {title: 'Fools Garden - Lemon Tree', file: 'music/28_Fools Garden - Lemon Tree.mp3', length: '3:09'},
+        {title: 'Johann Pachelbel - Kanon in D-Dur', file: 'music/28_Johann Pachelbel - Kanon in D-Dur.mp3', length: '4:32'}
+    ],
     'A': [
-        {'title': 'Dem Bach sein Song', 'file': 'music/test1.ogg', 'length': '9:59'},
-        {'title': 'Hein Blöd und das Bielefelder Symphonieorchester', 'file': 'music/test2.mp3', 'length': '8:59'}
+        {'title': 'Barenaked Ladies - The Big Bang Theory', 'file': 'music/A_Barenaked Ladies - The Big Bang Theory.mp3', 'length': '1:44'},
+    ],
+    'B': [{title: 'Louis Armstrong - What a wonderful world', file: 'music/B_Louis Armstrong - What a wonderful world.mp3', length: '2:19'}],
+    'C': [{title: 'Ludwig van Beethoven - Sinfonie 9 - Satz 4', file: 'music/C_Ludwig van Beethoven - Sinfonie 9 - Satz 4.mp3', length: '23:40'}],
+    'D': [{title: 'Alex Clare - Whispering', file: 'music/D_Alex Clare - Whispering.mp3', length: '4:30'}],
+    'E': [{
+        title: 'Richard Wagner - Parsifal Akt 1 - Wein und Brot des letzten Mahles',
+        file: 'music/E_Richard Wagner - Parsifal Akt 1 - Wein und Brot des letzten Mahles.mp3',
+        length: '12:14'
+    }],
+    'F': [{
+        title: 'Pjotr Iljitsch Tschaikowski - Schwanensee - Tanz der Schwäne',
+        file: 'music/F_Pjotr Iljitsch Tschaikowski - Schwanensee - Tanz der Schwäne.mp3',
+        length: '7:26'
+    }],
+    'G': [{
+        title: 'Johann Sebastian Bach - Toccata und Fugue in d Moll',
+        file: 'music/G_Johann Sebastian Bach - Toccata und Fugue in d Moll.mp3',
+        length: '9:46'
+    }],
+    'H': [
+        {title: 'Ludwig van Beethoven - Sinfonie 5 - Satz 1', file: 'music/H_Ludwig van Beethoven - Sinfonie 5 - Satz 1.mp3', length: '7:21'},
+        {title: 'Michael Jackson - Beat it', file: 'music/H_Michael Jackson - Beat it.mp3', length: '4:18'},
+        {title: 'Wolfgang Amadeus Mozart - Eine kleine Nachtmusik', file: 'music/H_Wolfgang Amadeus Mozart - Eine kleine Nachtmusik.mp3', length: '5:44'}
     ]
 };
 
