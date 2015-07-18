acdc1c17b17fcf75f79c7f33471bf20b259332fd
Andreas Hemel <dai.shan@gmx.net>
2015-07-18 15:33:08 +0200

    css debug mode

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo '$debug: false;' > app/styles/_debug.scss
 brunch build --production
 rsync -av --delete public/ daishan.de:~/public_html/schopenhauer/
 brunch build
+echo '$debug: true;' > app/styles/_debug.scss
